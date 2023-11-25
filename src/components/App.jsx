0c65c42316570b35a40b895c9c642620c80971eb
Ankit Kaushik <ankitkaushikx@gmail.com>
2023-11-25 15:29:51 +0530

    EWF- Add New Notes , Delete Notes Functionality Implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Note from "../components/Note";
-import notesData from "../notes.json";
+
+import CreateArea from "./CreateArea";
 function App() {
+  const [noteArray, setNoteArray] = useState([
+    {
+      title: "My New Note",
+      content: "Content For My New Note",
+    },
+  ]);
+
+  function addNewNote(newNoteItem) {
+    setNoteArray((prevNoteArray) => {
+      return [...prevNoteArray, newNoteItem];
+    });
+  }
+  function deleteNote(itemIndex) {
+    setNoteArray((prevNoteArray) => {
+      return prevNoteArray.filter((item, index) => {
+        return index != itemIndex;
+      });
+    });
+  }
   return (
     <div>
       <Header />
-      {notesData.map((note, index) => {
-        return <Note key={index} title={note.title} content={note.content} />;
+      <CreateArea onClicked={addNewNote} />
+      {noteArray.map((noteItem, index) => {
+        return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onClicked={deleteNote} />;
       })}
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 function Note(props) {
   return (
-    <div className="note">
+    <div className="note" id={props.id}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <button
+        onClick={(event) => {
+          props.onClicked(props.id);
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 }
